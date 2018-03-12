999f278b2dcfe6dc9b41ea6c8622b8d53c12fdb7
Rui Lopes <ruieduardo.fa.lopes@av.it.pt>
2018-03-12 22:07:20 +0000

    Added complete documentation of the Horse/Jockey's state transitions

--- entities/HorseJockeyState.java
+++ entities/HorseJockeyState.java
@@ -1,14 +1,40 @@
 package entities;
 
+import hippodrome.ControlCentre;
+import hippodrome.Paddock;
+
 /**
  * Enumeration of all pair Horse/Jockey's states, given on its lifecycle.
  *
  * @see HorseJockey#run()
  */
 public enum HorseJockeyState {
+    /**
+     * This is a <strong>blocking state</strong>. Here, the pair Horse/Jockey ({@link HorseJockey}) is waken up
+     * by one of the following operations of the {@link Broker}: {@link hippodrome.Stable#summonHorsesToPaddock(int)}
+     * during the races, or {@link ControlCentre#entertainTheGuests()}, at the end.
+     */
     AT_THE_STABLE,
+    /**
+     * This is a <strong>blocking state</strong>. Here, the pair Horse/Jockey is waken up by the
+     * operation {@link Paddock#goCheckHorses()} of the last {@link Spectator} to reach the {@link Paddock}.
+     */
     AT_THE_PADDOCK,
+    /**
+     * This is a <strong>blocking state</strong>. Here, the pair Horse/Jockey is waken up by the
+     * operation {@link ControlCentre#startTheRace(int)} of the {@link Broker} (the first) or by the
+     * operation {@link hippodrome.RacingTrack#makeAMove(HorseJockey)} of another pair Horse/Jockey.
+     */
     AT_THE_START_LINE,
+    /**
+     * This is a <strong>blocking state</strong> with transition. Here, the pair Horse/Jockey
+     * <strong>blocks</strong> after carrying out a position increment, unless he crosses the finishing line.
+     *
+     * <strong>Note that he always wakes up the next pair Horse/Jockey that has not completed the race yet!</strong>
+     */
     RUNNING,
+    /**
+     * This is a transition state.
+     */
     AT_THE_FINNISH_LINE
 }
