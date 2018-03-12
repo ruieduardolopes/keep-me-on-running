d2e953c77f993495768adcdec422b6debcd62129
Rui Lopes <ruieduardo.fa.lopes@av.it.pt>
2018-03-12 23:13:23 +0000

    Added incomplete version of a concepts package

---
+++ concepts/Race.java
@@ -0,0 +1,26 @@
+package concepts;
+
+/**
+ *
+ */
+public class Race {
+    /**
+     *
+     * @param numberOfTracks
+     * @param identification
+     */
+    public Race(int numberOfTracks, int identification) {
+        this.numberOfTracks = numberOfTracks;
+        this.identification = identification;
+    }
+
+    /**
+     *
+     */
+    private int numberOfTracks;
+
+    /**
+     *
+     */
+    private int identification;
+}
---
+++ concepts/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Provides the extra classes necessary to create a
+ * perfect development of a horse race scenario.
+ *
+ * @author Hugo Fragata
+ * @author Rui Lopes
+ * @see entities
+ * @see hippodrome
+ * @see stage
+ * @since 1.0
+ */
+package concepts;
