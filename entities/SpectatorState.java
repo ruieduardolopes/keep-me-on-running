38034a788fe2d2521d7d1f79f70a95b04e3d46c9
Rui Lopes <ruieduardo.fa.lopes@av.it.pt>
2018-03-12 22:21:49 +0000

    Added complete documentation of the Spectator's state transitions

--- entities/SpectatorState.java
+++ entities/SpectatorState.java
@@ -1,15 +1,45 @@
 package entities;
 
+import hippodrome.ControlCentre;
+import hippodrome.RacingTrack;
+
 /**
  * Enumeration of all Spectator's states, given on its lifecycle.
  *
  * @see Spectator#run()
  */
 public enum SpectatorState {
+    /**
+     * This is a <strong>blocking state</strong>. Here, the {@link Spectator} is waken up by the operation
+     * {@link hippodrome.Paddock#proceedToPaddock(int)} of the last pair Horse/Jockey to reach the {@link hippodrome.Paddock}.
+     */
     WAITING_FOR_A_RACE_TO_START,
+    /**
+     * This is a <strong>blocking state</strong>. Here, the {@link Spectator} is waken up by the operation
+     * {@link RacingTrack#proceedToStartLine()} of the pair Horse/Jockey to leave the {@link hippodrome.Paddock}.
+     */
     APPRAISING_THE_HORSES,
+    /**
+     * This is a <strong>blocking state</strong> with transition. Here, the {@link Spectator} blocks in queue
+     * while waiting to place the bet.
+     *
+     * Note that he or she is waken up by the {@link Broker} when the bet is done.
+     */
     PLACING_A_BET,
+    /**
+     * This is a <strong>blocking state</strong>. Here, the {@link Spectator} is waken up by the operation
+     * {@link ControlCentre#reportResults()} of the broker
+     */
     WATCHING_A_RACE,
+    /**
+     * This is a <strong>blocking state</strong> with transition. Here, the {@link Spectator} blocks in queue
+     * while waiting to receive the dividends.
+     *
+     * He or she is waken up by the {@link Broker} when the transaction is completed.
+     */
     COLLECTING_THE_GAINS,
+    /**
+     * This is the final state. This is a transition state.
+     */
     CELEBRATING
 }
