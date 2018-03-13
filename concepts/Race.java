577aa500d67d974483610050a681105bd19b1c65
Rui Lopes <ruieduardo.fa.lopes@av.it.pt>
2018-03-13 00:22:30 +0000

    Added documentation to the concepts.Race class

--- concepts/Race.java
+++ concepts/Race.java
@@ -1,13 +1,18 @@
 package concepts;
 
 /**
+ * Representation of a race, which is done over a {@link hippodrome.RacingTrack}.
  *
+ * @author Hugo Fragata
+ * @author Rui Lopes
+ * @since 1.0
  */
 public class Race {
     /**
+     * Constructs a race with {@code numberOfTracks} number of tracks and identification {@code identification}.
      *
-     * @param numberOfTracks
-     * @param identification
+     * @param numberOfTracks the number of tracks to be used on this race.
+     * @param identification the race number identification.
      */
     public Race(int numberOfTracks, int identification) {
         this.numberOfTracks = numberOfTracks;
@@ -15,12 +20,12 @@ public class Race {
     }
 
     /**
-     *
+     * The number of tracks this race will have in use.
      */
     private int numberOfTracks;
 
     /**
-     *
+     * This race identification number.
      */
     private int identification;
 }
