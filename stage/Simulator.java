94e6c93d8d6080ea4a848d53cb9c66734aefe1ce
Rui Lopes <ruieduardo.fa.lopes@av.it.pt>
2018-03-15 11:02:28 +0000

    Added documentation header for stage.Simulator class

--- stage/Simulator.java
+++ stage/Simulator.java
@@ -1,5 +1,10 @@
 package stage;
 
+/**
+ * Creation of a Horse Race simulation placed on a Hippodrome.
+ * This class has a {@link entities.Broker}, a set of {@link entities.Spectator}s
+ * and a set of pairs Horse/Jockey ({@link entities.HorseJockey}s).
+ */
 public class Simulator {
 
 }
