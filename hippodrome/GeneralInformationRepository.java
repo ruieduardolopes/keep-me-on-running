512ac7495fae1a580f6fc2de62c63fef1b474944
Rui Lopes <ruieduardo.fa.lopes@av.it.pt>
2018-03-18 21:43:01 +0000

    Added the printHeaders() function to print the headers on terminal and on a file

--- hippodrome/GeneralInformationRepository.java
+++ hippodrome/GeneralInformationRepository.java
@@ -1,6 +1,7 @@
 package hippodrome;
 
 import exceptions.InaccessibleFileException;
+import genclass.GenericIO;
 import genclass.TextFile;
 
 import java.time.Instant;
@@ -57,6 +58,21 @@ public class GeneralInformationRepository {
         if (!actionSucceeded) {
             throw new exceptions.InaccessibleFileException("The requested file \"" + filename + "\" is currently unaccessible or this user does not have permissions to write on this directory.");
         }
+
+        /* print the title */
+        String title = "AFTERNOON AT THE RACE TRACK - Description of the internal state of the problem";
+        GenericIO.writelnString(title);
+        file.writelnString(title);
+
+        /* print the column headers */
+        String header1 = "MAN/BRK           SPECTATOR/BETTER              HORSE/JOCKEY PAIR at Race RN\n" +
+                         "  Stat  St0  Am0 St1  Am1 St2  Am2 St3  Am3 RN St0 Len0 St1 Len1 St2 Len2 St3 Len3";
+        String header2 = "                                        Race RN Status\n" +
+                         " RN Dist BS0  BA0 BS1  BA1 BS2  BA2 BS3  BA3  Od0 N0 Ps0 SD0 Od1 N1 Ps1 Sd1 Od2 N2 Ps2 Sd2 Od3 N3 Ps3 St3";
+        GenericIO.writelnString(header1);
+        file.writelnString(header1);
+        GenericIO.writelnString(header2);
+        file.writelnString(header2);
     }
 
     private static String filename;
