59c12ec5e60276a15d38acc07c2b4aa589f73ce5
Rui Lopes <ruieduardo.fa.lopes@av.it.pt>
2018-03-18 22:56:58 +0000

    Added package-info to registry package

---
+++ hippodrome/registry/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Provides the classes which saves a snapshot of both
+ * entities at stake and the race.
+ *
+ * @author Hugo Fragata
+ * @author Rui Lopes
+ * @see hippodrome.actions
+ * @since 0.1
+ * @version 0.1
+ */
+package hippodrome.registry;
