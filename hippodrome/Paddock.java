672c5f81d45c0ac17bf42a93359dc41370e1df15
Rui Lopes <ruieduardo.fa.lopes@me.com>
2018-03-25 19:10:12 +0100

    Reviewed the documentation of the Paddock

--- hippodrome/Paddock.java
+++ hippodrome/Paddock.java
@@ -17,20 +17,41 @@ import hippodrome.actions.Race;
  * @version 1.0
  */
 public class Paddock {
+    /**
+     * Creates a Racing Track.
+     * <br>
+     * This constructor creates a Racing Track giving a race. Plus, an instance of the
+     * repository is also given in order to report status changes on the course of its actions.
+     *
+     * @param race A race to be executed over this Racing Track.
+     * @param repository An instance of a {@link GeneralInformationRepository} in order to report all the actions and
+     *                   log each and every moment.
+     */
+
+    /**
+     * Creates a Paddock.
+     * <br>
+     * This constructor creates a Paddock giving a number of pairs Horse/Jockey and a number of Spectators. Plus,
+     * an instance of the repository is also given in order to report status changes on the course of its actions.
+     *
+     * @param numberOfSpectators The number of Spectators which will be attending the event.
+     * @param numberOfHorses The number of pairs Horse/Jockey which will be competing.
+     * @param repository An instance of a {@link GeneralInformationRepository} in order to report all the actions and
+     *                   log each and every moment.
+     */
     public Paddock(int numberOfSpectators, int numberOfHorses, GeneralInformationRepository repository) {
         this.numberOfSpectators = numberOfSpectators;
         this.numberOfHorses = numberOfHorses;
         this.repository = repository;
     }
+
     /**
-     * Signal given by the {@link entities.Broker} in order to proceed to here (the {@link Paddock}).
-     * This signal is given calling only the pairs Horse/Jockey which are supposed to run on the next {@link Race},
-     * identified by the number {@code raceNumber}.
+     * Changes the state of the pair Horse/Jockey to At the Paddock ({@code ATP}) and waits for the last Spectator to
+     * arrive on Paddock on the {@link Paddock#goCheckHorses(boolean)} method.
+     * <br>
+     * This method also resets the condition variable of the {@link Paddock#goCheckHorses(boolean)} method.
      *
      * @param raceNumber number identification of the race which is about to begin.
-     *
-     * @return {@code true} if the last pair Horse/Jockey has been moved to the {@link Paddock}; otherwise
-     * {@code false}.
      */
     public synchronized void proceedToPaddock(int raceNumber) {
         HorseJockey horse = ((HorseJockey)Thread.currentThread());
@@ -60,6 +81,15 @@ public class Paddock {
      * @param isTheLastSpectator {@code boolean} variable which identifies when the last {@link entities.Spectator}
      *                           has reached the premises.
      */
+
+    /**
+     * Changes the Spectator's state to Appraising the Horses ({@code ATH}) and waits till the last
+     * {@link Paddock#proceedToStartLine()}. It also notifies {@link Paddock#proceedToPaddock(int)} when the last
+     * Spectator is about to appraise the horse - you fool.
+     *
+     * @param isTheLastSpectator identification if the last {@link entities.Spectator}
+     *                           has reached the premises - if {@code true}; otherwise {@code false}.
+     */
     public synchronized void goCheckHorses(boolean isTheLastSpectator) {
         ((Spectator)Thread.currentThread()).setSpectatorState(SpectatorState.APPRAISING_THE_HORSES);
         if (isTheLastSpectator) {
@@ -81,6 +111,12 @@ public class Paddock {
         lastSpectatorHasNotArrivedOnPaddock = true;
     }
 
+    /**
+     * Signal that the race is about to start, by a {@link entities.HorseJockey}.
+     * <br>
+     * Note that this method changes the value of the condition variable to the {@link Paddock#goCheckHorses(boolean)}
+     * wait condition, notifying its changes.
+     */
     public synchronized void proceedToStartLine() {
         currentNumberOfSpectators = 0;
         currentNumberOfHorses++;
@@ -104,12 +140,6 @@ public class Paddock {
         return currentNumberOfSpectators == numberOfSpectators;
     }
 
-
-    /**
-     * Instance of the global repository of information given by the {@code Simulation}.
-     */
-    private GeneralInformationRepository repository;
-
     /**
      * Current number of {@link HorseJockey} present in the {@link Paddock}.
      */
@@ -121,22 +151,41 @@ public class Paddock {
     private int currentNumberOfSpectators;
 
     /**
-     * Total number of {@link HorseJockey} present in the {@link Race}.
+     * Total number of {@link HorseJockey} competing.
      */
     private int numberOfHorses;
 
     /**
-     * Total number of {@link Spectator} present in the {@link Race}.
+     * Total number of {@link Spectator} attending the events.
      */
     private int numberOfSpectators;
 
+
     /**
-     * Indicates whether all the {@link HorseJockey}s have proceeded to the {@link RacingTrack}.
+     * Condition variable for noticing when a race is about to end.
+     * <br>
+     * This is a condition variable of {@link RacingTrack#makeAMove(int)}.
+     */
+
+    /**
+     * Condition variable for noticing when the last pair Horse/Jockey have (or have not) proceeded to the
+     * start line.
+     *
+     * This is a condition variable of the {@link Paddock#goCheckHorses(boolean)} method and it is reset on the
+     * {@link Paddock#proceedToPaddock(int)} method.
      */
     private boolean lastHorseDidNotProceedToStartLine = true;
 
     /**
-     * Indicates whether all the {@link Spectator}s have proceeded to the {@link Paddock}.
+     * Condition variable for noticing when the last Spectator has (or has not) arrived to the Paddock.
+     *
+     * This is a condition variable of the {@link Paddock#proceedToPaddock(int)} method and it is reset on the
+     * {@link Paddock#goCheckHorses(boolean)} method.
      */
     private boolean lastSpectatorHasNotArrivedOnPaddock = true;
+
+    /**
+     * The {@link GeneralInformationRepository} instance where all this region's actions will be reported.
+     */
+    private GeneralInformationRepository repository; // TODO - is it needed?
 }
