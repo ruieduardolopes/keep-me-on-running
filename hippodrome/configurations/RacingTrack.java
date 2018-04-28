7b3a2d4ac21d794ba2c3df0b9c8d755590f1fa9e
Rui Lopes <ruieduardo.fa.lopes@me.com>
2018-04-28 19:56:09 +0100

    Added the Racing Track configuration, but notice that two of three items on  it must be changed

--- hippodrome/configurations/RacingTrack.java
+++ hippodrome/configurations/RacingTrack.java
@@ -1,4 +1,7 @@
 package hippodrome.configurations;
 
 public class RacingTrack {
+    public static final int NUMBER_OF_TRACKS = 4;
+    public static final int RACE_IDENTIFICATION = 0; // TODO : this should not be a constant at all
+    public static final int RACE_DISTANCE = 100; // TODO : this should not be a constant at all
 }
