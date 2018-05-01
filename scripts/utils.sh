bb0c1dff515237a117f88bcd5796153071529d18
Rui Lopes <ruieduardo.fa.lopes@me.com>
2018-05-01 23:33:44 +0100

    Added script to update the code from the repo to the server

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -18,7 +18,7 @@ revertsim () {
 
     # Verification of the result
     if [[ "$?" -ne 0 ]]; then
-        echo "Unable to replace the Race's distance to $1 as minimum and $2 as maximum." >&2
+        echo "Unable to revert the configurations of made to the simulator." >&2
         cd -
         return 2
     fi
@@ -28,4 +28,35 @@ revertsim () {
 
 print_revertsim_usage () {
     echo "Usage: revertsim"
+}
+
+# Method to update the code on the repository.
+#
+updcode () {
+    if [[ "$1" -eq '-h' && "$#" -eq 1 ]]; then
+        print_updcode_usage
+        return 0
+    fi
+    if [ "$#" -ne 0 ]; then
+        echo "Options not recognized." >&2
+        print_updcode_usage
+        return 1
+    fi
+
+    cd $WORK_PATH
+    git checkout --force tests
+    git pull origin tests
+
+    # Verification of the result
+    if [[ "$?" -ne 0 ]]; then
+        echo "Unable to update the code from the repository." >&2
+        cd -
+        return 2
+    fi
+    cd -
+    return 0
+}
+
+print_updcode_usage () {
+    echo "Usage: updcode"
 }
