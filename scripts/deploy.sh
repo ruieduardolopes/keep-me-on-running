6932099969494b12aba9d9410563d035c2a693df
Rui Lopes <ruieduardo.fa.lopes@me.com>
2018-05-01 20:53:18 +0100

    Added script for initialization and to simple deploy

---
+++ scripts/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+launchhorses () {
+    for node in {01,02,03,04,05,06,07,08,09}; do
+        ssh sd0402@l040101-ws$node.ua.pt 'git pull origin tests'
+    done
+}
---
+++ scripts/init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+WORK_PATH=$(dirname "$(pwd)")
+
+source simulation-controls.sh
+source server-and-client-controls.sh
