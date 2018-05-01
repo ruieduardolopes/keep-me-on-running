68945c17996b0bd7bedd591f1d2595da427bd7bd
Rui Lopes <ruieduardo.fa.lopes@me.com>
2018-05-01 20:38:06 +0100

    Added the latter methods needed to control the simulation

--- scripts/simulation-controls.sh
+++ scripts/simulation-controls.sh
@@ -101,4 +101,98 @@ print_chraces_usage () {
     echo "  -t   Produce the same change to NUMBER_OF_TRACKS (the number of tracks on a racing track)."
 }
 
+# Method to change the ability of the pair Horse/Jockey on the simulations.
+#
+# Parameters:
+#     $1 - is the new minimum ability.
+#     $2 - is the new maximum ability.
+#
+chability () {
+    if [[ "$1" -eq '-h' && "$#" -eq 1 ]]; then
+        print_chability_usage
+        return 0
+    fi
+    if [ "$#" -ne 2 ]; then
+        echo "Options not recognized." >&2
+        print_chability_usage
+        return 1
+    fi
 
+    sed -ie 's/ABILITY_MIN_BOUND.*/ABILITY_MIN_BOUND = $1;/g' configurations/SimulationConfigurations.java
+    sed -ie 's/ABILITY_MAX_BOUND.*/ABILITY_MAX_BOUND = $1;/g' configurations/SimulationConfigurations.java
+
+    # Verification of the result
+    if [[ "$?" -ne 0 ]]; then
+        echo "Unable to replace the Pair Horse/Jockey's ability to $1 as minimum and $2 as maximum." >&2
+        return 2
+    fi
+    return 0
+}
+
+print_chability_usage () {
+    echo "Usage: chability <minimum-ability> <maximum-ability>"
+}
+
+# Method to change the amount of money of the Spectators on the simulations.
+#
+# Parameters:
+#     $1 - is the new minimum amount of money.
+#     $2 - is the new maximum amount of money.
+#
+chmoney () {
+    if [[ "$1" -eq '-h' && "$#" -eq 1 ]]; then
+        print_chmoney_usage
+        return 0
+    fi
+    if [ "$#" -ne 2 ]; then
+        echo "Options not recognized." >&2
+        print_chmoney_usage
+        return 1
+    fi
+
+    sed -ie 's/MONEY_MIN_BOUND.*/MONEY_MIN_BOUND = $1;/g' configurations/SimulationConfigurations.java
+    sed -ie 's/MONEY_MAX_BOUND.*/MONEY_MAX_BOUND = $1;/g' configurations/SimulationConfigurations.java
+
+    # Verification of the result
+    if [[ "$?" -ne 0 ]]; then
+        echo "Unable to replace the Spectator's amount of money to $1 as minimum and $2 as maximum." >&2
+        return 2
+    fi
+    return 0
+}
+
+print_chmoney_usage () {
+    echo "Usage: chmoney <minimum-amount-of-money> <maximum-amount-of-money>"
+}
+
+# Method to change the distance of the Races on the simulations.
+#
+# Parameters:
+#     $1 - is the new minimum distance.
+#     $2 - is the new maximum distance.
+#
+chdistance () {
+    if [[ "$1" -eq '-h' && "$#" -eq 1 ]]; then
+        print_chdistance_usage
+        return 0
+    fi
+    if [ "$#" -ne 2 ]; then
+        echo "Options not recognized." >&2
+        print_chdistance_usage
+        return 1
+    fi
+
+    sed -ie 's/TRACK_DISTANCE_MIN_BOUND.*/TRACK_DISTANCE_MIN_BOUND = $1;/g' configurations/SimulationConfigurations.java
+    sed -ie 's/TRACK_DISTANCE_MAX_BOUND.*/TRACK_DISTANCE_MAX_BOUND = $1;/g' configurations/SimulationConfigurations.java
+
+    # Verification of the result
+    if [[ "$?" -ne 0 ]]; then
+        echo "Unable to replace the Race's distance to $1 as minimum and $2 as maximum." >&2
+        return 2
+    fi
+    return 0
+}
+
+print_chdistance_usage () {
+    echo "Usage: chdistance <minimum-distance> <maximum-distance>"
+}
