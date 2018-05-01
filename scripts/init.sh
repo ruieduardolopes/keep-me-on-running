34255f0df2a48c46a064efba4bb4f19959a82f69
Rui Lopes <ruieduardo.fa.lopes@me.com>
2018-05-01 21:12:25 +0100

    Added new scripts

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WORK_PATH=/home/keep-me-on-running
+WORK_PATH=/home/sd0402/keep-me-on-running
 
 source $WORK_PATH/scripts/simulation-controls.sh
 source $WORK_PATH/scripts/server-and-client-controls.sh
