5899c05a53c4d85ed6f7fc7b880c31a34248f559
Rui Lopes <ruieduardo.fa.lopes@me.com>
2018-05-06 03:20:39 +0100

    Added handling on the ServerLauncher

--- server/ServerLauncher.java
+++ server/ServerLauncher.java
@@ -10,7 +10,8 @@ import java.net.SocketTimeoutException;
 public class ServerLauncher {
     public static void main(String[] args) {
         if (args.length != 1) {
-            // TODO: handle this case
+            printUsage();
+            System.exit(1);
         }
         try {
             switch (args[0]) {
@@ -39,8 +40,8 @@ public class ServerLauncher {
                     port = ServerConfigurations.STABLE_PORT;
                     break;
                 default :
-                    // TODO : handle this case
-                    break;
+                    printUsage();
+                    System.exit(2);
             }
             Logger.printNotification("Preparing to run %s server on port %d", args[0], port);
             serverConnectionRequest = new ServerCom(port);
@@ -54,7 +55,9 @@ public class ServerLauncher {
                     Logger.printInformation("An agent was already made available to attend the situation");
                     serviceProviderAgent.start();
                 } catch (SocketTimeoutException ste) {
-                    // TODO : handle this exception
+                    Logger.printError("An exception has been thrown... catch it man! Below there is some information about it");
+                    ste.printStackTrace();
+                    System.exit(3);
                 }
                 terminateExecution = ServiceProviderAgent.getShutdownCounter(args[0]);
             }
@@ -62,6 +65,18 @@ public class ServerLauncher {
             Logger.printError("An exception has been thrown... catch it man! Below there is some information about it");
             ie.printStackTrace();
         }
+    }
+
+    private static void printUsage() {
+        System.err.println("Invalid arguments.");
+        System.err.println("Usage: java -classpath \"project-folder:genclass-jar-folder\" ServerLauncher <hippodrome-region>");
+        System.err.println("Hippodrome Regions:");
+        System.err.println("  - betting-centre      (Betting Centre)" +
+                "  - control-centre      (Control Centre)" +
+                "  - general-repo        (General Repository of Information)" +
+                "  - paddock             (Paddock)" +
+                "  - racing-track        (Racing Track)" +
+                "  - stable              (Stable)");
     }
 
     private static boolean terminateExecution = false;
