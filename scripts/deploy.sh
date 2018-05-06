a9e4f18d31205f3c8548ee47f09b71972569f1de
Rui Lopes <ruieduardo.fa.lopes@me.com>
2018-05-06 23:43:00 +0100

    Added diagram and fixed some issues on the scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+updatecoderemote () {
+    for node in {01,02,03,04,05,06,07,08,09}; do
+        ssh sd0402@l040101-ws$node.ua.pt 'updcode'
+    done
+}
 preparehippodrome () {
     for node in {01,02,03,04,05,06,07,08,09}; do
         ssh sd0402@l040101-ws$node.ua.pt 'updcode; compile_code'
