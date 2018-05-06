49291dbeeda27f2ffc0be7b243b9861b6fb46bda
Rui Lopes <ruieduardo.fa.lopes@me.com>
2018-05-06 17:34:59 +0100

    Added documentation for the package communication

--- communications/package-info.java
+++ communications/package-info.java
@@ -1,2 +1,16 @@
-/** TODO : documentation */
+/**
+ * Provides the classes necessary to represent any messages to be shared between entities and hippodrome regions.
+ *
+ * Its contents are a {@link clients.BettingCentreStub}, a
+ * {@link clients.ControlCentreStub}, a {@link clients.PaddockStub},
+ * a {@link clients.GeneralInformationRepositoryStub}, a {@link clients.RacingTrackStub}
+ * and a {@link clients.StableStub}.
+ *
+ * @author Hugo Fragata
+ * @author Rui Lopes
+ * @see server
+ * @see entities
+ * @since 2.0
+ * @version 2.0
+ */
 package communications;
