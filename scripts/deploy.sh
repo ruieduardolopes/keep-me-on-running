b3ef94d7ed90b0adb951951b2ffc1085ab6d7493
Rui Lopes <ruieduardo.fa.lopes@me.com>
2018-05-06 20:06:42 +0100

    Updated scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,42 @@
 #!/bin/bash
 
-launchhorses () {
+preparehippodrome () {
     for node in {01,02,03,04,05,06,07,08,09}; do
-        ssh sd0402@l040101-ws$node.ua.pt 'updcode'
+        ssh sd0402@l040101-ws$node.ua.pt 'updcode; compile_code'
     done
+    ssh sd0402@l040101-ws01.ua.pt 'chhost general-repo l040101-ws01.ua.pt'
+    ssh sd0402@l040101-ws02.ua.pt 'chhost betting-centre l040101-ws02.ua.pt'
+    ssh sd0402@l040101-ws03.ua.pt 'chhost control-centre l040101-ws03.ua.pt'
+    ssh sd0402@l040101-ws04.ua.pt 'chhost paddock l040101-ws04.ua.pt'
+    ssh sd0402@l040101-ws05.ua.pt 'chhost racing-track l040101-ws05.ua.pt'
+    ssh sd0402@l040101-ws06.ua.pt 'chhost stable l040101-ws06.ua.pt'
+    ssh sd0402@l040101-ws07.ua.pt 'chhost broker l040101-ws07.ua.pt'
+    ssh sd0402@l040101-ws08.ua.pt 'chhost spectators l040101-ws08.ua.pt'
+    ssh sd0402@l040101-ws09.ua.pt 'chhost horses l040101-ws09.ua.pt'
+    ssh sd0402@l040101-ws01.ua.pt 'chport general-repo 22411'
+    ssh sd0402@l040101-ws02.ua.pt 'chport betting-centre 22412'
+    ssh sd0402@l040101-ws03.ua.pt 'chport control-centre 22413'
+    ssh sd0402@l040101-ws04.ua.pt 'chport paddock 22414'
+    ssh sd0402@l040101-ws05.ua.pt 'chport racing-track 22415'
+    ssh sd0402@l040101-ws06.ua.pt 'chport stable 22416'
+    ssh sd0402@l040101-ws07.ua.pt 'chport broker 22417'
+    ssh sd0402@l040101-ws08.ua.pt 'chport spectators 22418'
+    ssh sd0402@l040101-ws09.ua.pt 'chport horses 22419'
+}
+
+execute_code () {
+    ssh sd0402@l040101-ws01.ua.pt 'execserver general-repo'
+    ssh sd0402@l040101-ws02.ua.pt 'execserver betting-centre'
+    ssh sd0402@l040101-ws03.ua.pt 'execserver control-centre'
+    ssh sd0402@l040101-ws04.ua.pt 'execserver paddock'
+    ssh sd0402@l040101-ws05.ua.pt 'execserver racing-track'
+    ssh sd0402@l040101-ws06.ua.pt 'execserver stable'
+    ssh sd0402@l040101-ws07.ua.pt 'execclient broker'
+    ssh sd0402@l040101-ws08.ua.pt 'execclient spectators'
+    ssh sd0402@l040101-ws09.ua.pt 'execclient horses'
+}
+
+deployall () {
+    preparehippodrome
+    execute_code
 }
--- scripts/utils.sh
+++ scripts/utils.sh
@@ -61,3 +61,103 @@ updcode () {
 print_updcode_usage () {
     echo "Usage: updcode"
 }
+
+# Method to update the code on the repository.
+#
+compile_code () {
+    if [[ "$1" -eq '-h' && "$#" -eq 1 ]]; then
+        print_compile_code_usage
+        return 0
+    fi
+    if [ "$#" -ne 0 ]; then
+        echo "Options not recognized." >&2
+        print_compile_code_usage
+        return 1
+    fi
+
+    cd $WORK_PATH
+    javac -cp "lib/genclass.jar:" clients/ClientLauncher.java
+
+    # Verification of the result
+    if [[ "$?" -ne 0 ]]; then
+        echo "Unable to compile the code." >&2
+        cd -
+        return 2
+    fi
+
+    javac -cp "lib/genclass.jar:" server/ServerLauncher.java
+
+    # Verification of the result
+    if [[ "$?" -ne 0 ]]; then
+        echo "Unable to compile the code." >&2
+        cd -
+        return 3
+    fi
+    cd -
+    return 0
+}
+
+print_compile_code_usage () {
+    echo "Usage: compile_code"
+}
+
+# Method to execute the server.
+#
+execserver () {
+    if [[ "$1" -eq '-h' && "$#" -eq 1 ]]; then
+        print_execserver_usage
+        return 0
+    fi
+    if [[ "$#" -ne 1 ]]; then
+        echo "Options not recognized." >&2
+        print_execserver_usage
+        return 1
+    fi
+
+    cd $WORK_PATH
+    java -cp "lib/genclass.jar:" server.ServerLauncher "$1"
+
+    # Verification of the result
+    if [[ "$?" -ne 0 ]]; then
+        echo "Unable to run the code." >&2
+        cd -
+        return 2
+    fi
+    cd -
+    return 0
+}
+
+print_execserver_usage () {
+    echo "Usage: execserver <hippodrome-region>"
+}
+
+# Method to execute the client.
+#
+execclient () {
+    if [[ "$1" -eq '-h' && "$#" -eq 1 ]]; then
+        print_execclient_usage
+        return 0
+    fi
+    if [[ "$#" -ne 1 ]]; then
+        echo "Options not recognized." >&2
+        print_execclient_usage
+        return 1
+    fi
+
+    cd $WORK_PATH
+    java -cp "lib/genclass.jar:" clients.ClientLauncher "$1"
+
+    # Verification of the result
+    if [[ "$?" -ne 0 ]]; then
+        echo "Unable to run the code." >&2
+        cd -
+        return 2
+    fi
+    cd -
+    return 0
+}
+
+print_execclient_usage () {
+    echo "Usage: execclient <entity>"
+}
+
