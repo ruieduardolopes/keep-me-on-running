edad71dc46a9476e59dedd674d9e90f1451c9bd9
Rui Lopes <ruieduardo.fa.lopes@me.com>
2018-06-01 16:56:21 +0100

    Added the package responses on hippodrome

---
+++ hippodrome/responses/Response.java
@@ -0,0 +1,10 @@
+package hippodrome.responses;
+
+import entities.Spectator;
+import entities.SpectatorState;
+
+public class Response {
+    public Response(ResponseType state, int value, SpectatorState newState) {
+        if (state == SpectatorState.PLACING_A_BET)
+    }
+}
---
+++ hippodrome/responses/ResponseType.java
@@ -0,0 +1,5 @@
+package hippodrome.responses;
+
+public enum ResponseType {
+    BETTING_CENTRE_PLACE_A_BET
+}
