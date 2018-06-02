1a50f1764349ce5261b793c67179d7d0059a4293
Rui Lopes <ruieduardo.fa.lopes@me.com>
2018-06-02 19:51:56 +0100

    Added entire list of responses to discard while composing the solution

--- hippodrome/responses/Response.java
+++ hippodrome/responses/Response.java
@@ -1,10 +1,31 @@
 package hippodrome.responses;
 
-import entities.Spectator;
+import entities.BrokerState;
 import entities.SpectatorState;
 
 public class Response {
-    public Response(ResponseType state, int value, SpectatorState newState) {
-        if (state == SpectatorState.PLACING_A_BET)
+    public Response(ResponseType type, BrokerState state) {
+        this.type = type;
+        this.brokerState = state;
     }
+
+    public Response(ResponseType type, SpectatorState state, int spectatorId, int value) {
+        this.type = type;
+        switch (this.type) {
+            case BETTING_CENTRE_PLACE_A_BET:
+                this.spectatorState = state;
+                this.spectatorId = spectatorId;
+                this.bet = value;
+                break;
+            default:
+                // TODO : handle this case
+                break;
+        }
+    }
+
+    private int bet = 0;
+    private int spectatorId = 0;
+    private ResponseType type = null;
+    private BrokerState brokerState = null;
+    private SpectatorState spectatorState = null;
 }
