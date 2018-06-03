5fe03afdd7b9eb7c0b6b8d55b44da993093b657d
Rui Lopes <ruieduardo.fa.lopes@me.com>
2018-06-03 01:25:49 +0100

    Added RMI interfaces for the Control Centre

--- hippodrome/responses/Response.java
+++ hippodrome/responses/Response.java
@@ -1,6 +1,7 @@
 package hippodrome.responses;
 
 import entities.BrokerState;
+import entities.HorseJockeyState;
 import entities.SpectatorState;
 import java.io.Serializable;
 
@@ -10,9 +11,22 @@ public class Response implements Serializable {
         this.brokerState = state;
     }
 
-    public Response(ResponseType type, SpectatorState state, int spectatorId, int value) {
+    public Response(ResponseType type, SpectatorState state, int spectatorId) {
         this.type = type;
         switch (this.type) {
+            case CONTROL_CENTRE_GO_WATCH_THE_RACE:
+                this.spectatorState = state;
+                this.spectatorId = spectatorId;
+                break;
+            default:
+                // TODO : handle this case
+                break;
+        }
+    }
+
+    public Response(ResponseType type, SpectatorState state, int spectatorId, int value) {
+        this.type = type;
+        switch (this.type) { // TODO : do the other cases
             case BETTING_CENTRE_PLACE_A_BET:
                 this.spectatorState = state;
                 this.spectatorId = spectatorId;
@@ -24,9 +38,38 @@ public class Response implements Serializable {
         }
     }
 
+    public Response(ResponseType type, SpectatorState state, int spectatorId, boolean value) {
+        this.type = type;
+        switch (this.type) { // TODO : verify the cases
+            case BETTING_CENTRE_PLACE_A_BET:
+                this.spectatorState = state;
+                this.spectatorId = spectatorId;
+                this.booleanValue = value;
+                break;
+            default:
+                // TODO : handle this case
+                break;
+        }
+    }
+
+    public Response(ResponseType type, HorseJockeyState state, int horseJockeyId) {
+        this.type = type;
+        switch (this.type) {
+            case CONTROL_CENTRE_PROCEED_TO_PADDOCK:
+                this.horseJockeyState = state;
+                this.horseJockeyId = horseJockeyId;
+                break;
+            default:
+                break;
+        }
+    }
+
     private int bet = 0;
     private int spectatorId = 0;
+    private int horseJockeyId = 0;
     private ResponseType type = null;
+    private boolean booleanValue = false;
     private BrokerState brokerState = null;
     private SpectatorState spectatorState = null;
+    private HorseJockeyState horseJockeyState = null;
 }
