487a6648e6c1cc40076bff800b5259134ef94b21
Rui Lopes <ruieduardo.fa.lopes@me.com>
2018-06-06 23:41:33 +0100

    Added aliases on init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,3 +5,7 @@ WORK_PATH=/home/sd0402/keep-me-on-running
 source $WORK_PATH/scripts/simulation-controls.sh
 source $WORK_PATH/scripts/server-and-client-controls.sh
 source $WORK_PATH/scripts/utils.sh
+
+alias 'java8'=java
+alias 'javac8'=javac
+alias 'rmiregistry8'=rmiregistry
