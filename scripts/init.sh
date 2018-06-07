be7dc6c5c328f5493e5d7e59c2db1ac3b87eae9d
Rui Lopes <ruieduardo.fa.lopes@me.com>
2018-06-07 16:54:13 +0100

    Added the last version on tests

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,6 +5,7 @@ WORK_PATH=/home/sd0402/keep-me-on-running
 source $WORK_PATH/scripts/simulation-controls.sh
 source $WORK_PATH/scripts/server-and-client-controls.sh
 source $WORK_PATH/scripts/utils.sh
+source $WORK_PATH/scripts/deploy.sh
 
 alias 'java8'=java
 alias 'javac8'=javac
